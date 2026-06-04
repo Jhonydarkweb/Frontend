2f47e5b1cbf26ae8b5628a84ec616468f06a9f79
JhonyxDarkWeb <camiloperrofeo@gmail.com>
2026-06-04 12:43:27 -0600

    Actualizar URL del Backend para produccion

--- src/Services/AuthService.js
+++ src/Services/AuthService.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { jwtDecode } from 'jwt-decode'
 
 export const client = axios.create({
-    baseURL: 'https://localhost:7098',
+    baseURL: 'https://backendinvestigacion.runasp.net',
 })
 
 export async function login({ email, password }) {
